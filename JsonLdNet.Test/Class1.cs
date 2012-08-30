b10988d41b8de4dfbee35995686abd41ad50be95
Ryan Roberts <ryansroberts@gmail.com>
2012-08-30 15:34:02 +0100

    Initial commit

---
+++ JsonLdNet.Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JsonLdNet.Test
+{
+    public class load_jsonld
+    {
+        [Fact]
+        public void WhenLoadingPayload()
+        {
+            
+        }
+    }
+}
---
+++ JsonLdNet/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace JsonLdNet
+{
+    public class Quad
+    {
+        
+    }
+
+    public class JsonJdOptions
+    {
+        public JsonJdOptions(Uri iri = null)
+        {
+            IRI = iri ?? new Uri("");
+            CompactArrays = true;
+        }
+
+        public Uri IRI { get; private set; }
+        public bool CompactArrays { get; set; }
+        public bool Optimize { get; set; }
+        public bool UseRdfType { get; set; }
+        public bool UseNativeTypes { get; set; }
+    }
+
+
+    public class JSonLdProcessor
+    {
+        public void ToRDF(JObject jsonld,Func<Quad> quadCallback,JsonJdOptions options )
+        {
+            
+        }
+    }
+}
